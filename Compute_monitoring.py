aed639d68ac9c343e27a51d65726490d3583a200
mandarha1 <124249535+mandarha1@users.noreply.github.com>
2023-03-09 13:34:08 +0530

    Update Compute_monitoring.py

--- Compute_monitoring.py
+++ Compute_monitoring.py
@@ -95,7 +95,7 @@ def wh_data(session):
         with col2:
             st.markdown("**:red[Monthly Credit Consumtpion - Graph]**")
             df_wh_month = raw_wh_df.filter(col("Year-Month") == data).collect()
-            df_wh_month = df_wh_month.group_by(col("WAREHOUSE_NAME")).agg(sum("CREDITS_USED").as_("Total Credit Consumption")).collect()
+            df_wh_month = df_wh_month.group_by(col("WAREHOUSE_NAME")).agg(sum("CREDITS_USED")).collect()
             st.table(df_wh_month)
             #st.bar_chart(df_wh_month,x="WAREHOUSE_NAME",y="Total Credit Consumption")
             #st.markdown("**:red[Warehouse Credit Consumtpion - Graph]**")
